bf86ddda0e7cf582c7f4c1986a23b80b343ee8a6
Jorge332 <jramirezp332@gmail.com>
2020-03-26 17:45:20 -0600

    To-do List app funciones basicas y local storage completos

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,28 +5,32 @@ import { Tasks } from "./interfaces";
   providedIn: "root"
 })
 export class TaskService {
-  myTasks = [];
+  myTasks;
 
   constructor() {
-    this.myTasks[0] = { name: "jorge", completed: false };
+    var local = localStorage.getItem('tasks');
+    this.myTasks = local != null ? JSON.parse(local): [];
   }
 
-  getTasks() {
-    return this.myTasks;
+  saveToLocal(){
+    localStorage.setItem('tasks',JSON.stringify(this.myTasks))
   }
 
   addTask(task) {
     this.myTasks.push(task);
+    this.saveToLocal();
   }
 
   deleteTask(id:number) {
     this.myTasks.splice(id,1);
+    this.saveToLocal();
   }
 
   markAsCompleted(id, completed) {
     var task = this.myTasks[id];
     task.completed = completed;
     this.myTasks[id] = task;
+    this.saveToLocal();
   }
 
   editTask() {}
